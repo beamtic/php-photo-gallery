8fbb3d6ecdbab2cd010245426acd9acf096591d7
Jacob <jacobseated@gmail.com>
2025-07-25 20:43:27 +0200

    rm md5 stuff

--- stringtomd5.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This file can be used to create a new md5 hash from a password.
-// In the future the Gallery will do this automatically
-
-if ((isset($_GET['string'])) && (strlen($_GET['string'])) <= 200) {
-    echo md5($_GET['string']);
-} else {
-  echo 'Max 200 characters...';
-}
